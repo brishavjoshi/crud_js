f875c1b2422f2a21620cebd3edffb56a3cd7aad6
brishavjoshi <brishavjoshi2081@gmail.com>
2026-02-03 21:01:09 +0545

    app.js added

--- javascript/app.js
+++ javascript/app.js
@@ -12,5 +12,15 @@ function render() {
   app.appendChild(itemsElement);
 }
 
-// Initialize App
-render();
+// ....
+
+// Edit Completed Function
+export function editCompleted(itemId) {
+  items = items.map((item) => {
+    if (item.id === itemId) {
+      return { ...item, completed: !item.completed };
+    }
+    return item;
+  });
+  render();
+}
